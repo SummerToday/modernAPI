1eda066524a9ec350066a704f9cff3910ba0778a
SummerToday <kom347@naver.com>
2025-01-28 18:02:39 +0900

    OAS 설계 및 작성

---
+++ src/main/java/com/example/modernAPI/ModernApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.modernAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/modernAPI/ModernApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modernAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
